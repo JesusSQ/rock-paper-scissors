7cd2f8d0f200f141ff4ce9144fcdbcf22bf412f0
JesusSQ <jesus04122@gmail.com>
2023-09-06 20:07:41 +0200

    Create a game function and edited the round function
    Create a game function to play a match of 5 rounds and added
    a boolean return statement to the round function that is true
    if the player wins, false if the player lose and null if there is
    a tie
    Also edit README

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # rock-paper-scissors
+This is a little project on javaScript to play this famous game.
+You can only play with the console for the moment but I'll make 
+an interactive version in the future.
+
+# piedra-papel-tijeras
+Este es un pequeño proyecto en javaScript para jugar a este famoso 
+juego.
+Solo se puede jugar en la consola por el momento pero haré una version interactiva en el futuro. 
--- game.js
+++ game.js
@@ -36,23 +36,30 @@ function roundOfGame() {
     if (playerChoice !== null) {
         if (playerChoice === computerChoice) {
             console.log("Thats a tie");
+            return null;
         } else if (playerChoice === "rock") {
             if (computerChoice === "scissors") {
                 console.log("You Win! Rock beats Scissors");
+                return true;
             } else {
                 console.log("You Lose! Paper beats Rock");
+                return false;
             }
         } else if (playerChoice === "paper") {
             if (computerChoice === "rock") {
                 console.log("You Win! Paper beats Rock");
+                return true;
             } else {
                 console.log("You Lose! Scissors beats Paper");
+                return false;
             }
         } else {
             if (computerChoice === "paper") {
                 console.log("You Win! Scissors beats Paper");
+                return true;
             } else {
                 console.log("You Lose! Rock beats Scissors");
+                return false;
             }
         }
     } else {
@@ -60,4 +67,24 @@ function roundOfGame() {
     }
 }
 
-roundOfGame();
+function game() {
+    let playerScore = 0;
+    let computerScore = 0;
+    for (let i = 0; i < 5; i++) {
+        let result = roundOfGame();
+        if (result === true) {
+            playerScore++;
+        } else if (result === false) {
+            computerScore++;
+        }
+    }
+    if (playerScore === computerScore) {
+        return "That's an absolutely tie!";
+    } else if (playerScore > computerScore) {
+        return "You have won the game. Congratulations!";
+    } else {
+        return "You have lost the game. Good luck next time!";
+    }
+}
+
+console.log(game());
