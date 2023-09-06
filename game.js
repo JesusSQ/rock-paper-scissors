234c1a7c492042629b9af39b6eed908112672955
JesusSQ <jesus04122@gmail.com>
2023-09-06 19:35:39 +0200

    Create roundOfGame function
    Create a conditional structure to determinate if the player has
    won, tied or lose
    Create a askForSelection function that lets the players type their
    selection.
    Create a loop to restart if the player enter an invalid option.

--- game.js
+++ game.js
@@ -4,16 +4,60 @@ function getComputerChoice() {
 
     switch (randomNumber) {
         case 0:
-            result = "Rock";
+            result = "rock";
             break;
         case 1:
-            result = "Paper"
+            result = "paper"
             break;
         case 2:
-            result = "Scissors";
+            result = "scissors";
     }
 
     return result;
 }
 
-console.log(getComputerChoice());
+function askForSelection() {
+    let answer = prompt("Type your choice", "");
+    let answerFormal = answer.toLowerCase();
+    if (answerFormal === "rock" ||
+        answerFormal === "paper" ||
+        answerFormal === "scissors") {
+        return answerFormal;
+    } else {
+        console.log("Invalid syntax");
+        return null;
+    }
+}
+
+function roundOfGame() {
+    let computerChoice = getComputerChoice();
+    let playerChoice = askForSelection();
+
+    if (playerChoice !== null) {
+        if (playerChoice === computerChoice) {
+            console.log("Thats a tie");
+        } else if (playerChoice === "rock") {
+            if (computerChoice === "scissors") {
+                console.log("You Win! Rock beats Scissors");
+            } else {
+                console.log("You Lose! Paper beats Rock");
+            }
+        } else if (playerChoice === "paper") {
+            if (computerChoice === "rock") {
+                console.log("You Win! Paper beats Rock");
+            } else {
+                console.log("You Lose! Scissors beats Paper");
+            }
+        } else {
+            if (computerChoice === "paper") {
+                console.log("You Win! Scissors beats Paper");
+            } else {
+                console.log("You Lose! Rock beats Scissors");
+            }
+        }
+    } else {
+        roundOfGame();
+    }
+}
+
+roundOfGame();
